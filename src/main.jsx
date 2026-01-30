887610e3370798fffad52f2610d77f950274622a
work-crosca <work.crosca@gmail.com>
2026-01-30 12:18:52 +0000

    fix styles and main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { registerSW } from "virtual:pwa-register";
 import "./styles/base.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+registerSW({ immediate: true });
 
 root.render(
   <StrictMode>
